f509f440a820a2090be07f0b770f5803c2ae4f44
caleb krueger <ckrueger117@gmail.com>
2023-08-11 15:53:03 -0500

    naming changes

--- GuiderAO/Maxim Scripting Interface/guider_interface.py
+++ GuiderAO/Maxim Scripting Interface/guider_interface.py
@@ -8,12 +8,12 @@ Included in the imports is the maxim_menu.py file which holds a library of pytho
 Using match/case (!!!a feature of Python 3.10 and later!!!) we can simulate a user...
 ...interface by continuously prompting the operator to execute a command. Until the...
 ...user has declined to execute another command, the session will be ongoing and the...
-...MaxIm.Application object will still be open.
+...MaxIm.Application Object will still be open.
 
 Commands:
-1. 'set' :establishes connection with the guider camera by initializing a ccd camera object.
+1. 'set' :establishes connection with the guider camera by initializing a ccd camera Object.
 
-2. 'sever' :disconnects the guider camera by de-initializing the ccd camera object.
+2. 'sever' :disconnects the guider camera by de-initializing the ccd camera Object.
 
 3. 'expose' :takes a light exposure with the guider camera; how guide stars are found.
     - Takes a float value for duration as a parameter.
@@ -21,12 +21,12 @@ Commands:
 4. 'calibrate' :begins the calibration process in MaxIm DL. Should be done on device startup.
     - Takes a float value for duration (of each exposure) as a parameter.
 
-5. 'calcode' :returns the present calibration code for the guider object.
+5. 'calcode' :returns the present calibration code for the guider Object.
     - Decoding table can be found in the scripting manual.
 
 6. 'stop' :instantaneously quits the guider's current task and returns the status to idle.
 
-7. 'status' :returns the present status code for the ccd camera object.
+7. 'status' :returns the present status code for the ccd camera Object.
     - Decoding table can be found in scripting manual.
 
 8. 'track' :begins the guide star tracking process in MaxIm DL. Guide star must alr be found.
@@ -47,8 +47,8 @@ options = ['set', 'sever', 'expose', 'calibrate', 'calcode', 'stop', 'status', '
            'starcoords', 'list', 'quit']
 
 session = 1 # 1 means interface open, 0 means interface closed
-object = None
-#the application object is initialized, opening MaxIm DL
+Object = None
+#the application Object is initialized, opening MaxIm DL
 app = comtypes.client.CreateObject("MaxIm.Application")
 
 #the session loop
@@ -56,46 +56,46 @@ while session == 1:
 
     command_string = input('Command?\n:') #must be typed lower case as one word
     match command_string:
-        case "set": #establishes a maxim.ccdcamera object
-            object = maxim_menu.guider_connect()
+        case "set": #establishes a maxim.ccdcamera Object
+            Object = maxim_menu.guider_connect()
             session = 1
             continue
-        case "sever": #quits the camera object, unsure if quits both
-            maxim_menu.guider_disconnect(object)
+        case "sever": #quits the camera Object, unsure if quits both
+            maxim_menu.guider_disconnect(Object)
             session = 1
             continue
         case "expose": #takes a guider exposure, will change when add normal expose
             duration = input('Duration in s? (float)\n:') #respond in float value
             duration = float(duration)
-            maxim_menu.guider_expose(duration, object)
+            maxim_menu.guider_expose(duration, Object)
             session = 1
             continue
         case "calibrate": #calibrates guider, dont think we'll have to add ccdcal
             duration = input('Duration in s? (float)\n:') #respond in float value
             duration = float(duration)
-            maxim_menu.guider_calibrate(object, duration)
+            maxim_menu.guider_calibrate(Object, duration)
             session = 1
             continue
         case "calcode": #returns the calibration code of the guider
-            maxim_menu.guider_calstate(object) # refer to manual for codes 
+            maxim_menu.guider_calstate(Object) # refer to manual for codes 
             session = 1
             continue
         case "starcoords": #returns coords of guide star in photo
-            maxim_menu.guidestar_coords(object)
+            maxim_menu.guidestar_coords(Object)
             session = 1
             continue
         case "track": #tracks the currently selected guidestar
             duration = input('Duration in s? (float)\n:') # respond in float value
             duration = float(duration)
-            maxim_menu.guider_track(object, duration)
+            maxim_menu.guider_track(Object, duration)
             session = 1
             continue
         case "stop": #stops guider tracking, returns cam to idle
-            maxim_menu.guider_stop(object)
+            maxim_menu.guider_stop(Object)
             session = 1
             continue
         case "status": #checks status of the ccd camera
-            maxim_menu.cam_status(object)
+            maxim_menu.cam_status(Object)
             session = 1
             continue
         case "list": #returns list of operable commands to the command line
--- GuiderAO/Maxim Scripting Interface/maxim_menu.py
+++ GuiderAO/Maxim Scripting Interface/maxim_menu.py
@@ -1,52 +1,52 @@
 import comtypes.client
 import time
 
-def guider_connect(): # creates the MaxIm.CCDCamera object
-    object = comtypes.client.CreateObject("MaxIm.CCDCamera")
+def guider_connect(): # creates the MaxIm.CCDCamera Object
+    Object = comtypes.client.CreateObject("MaxIm.CCDCamera")
     time.sleep(.5) #extra time to be safe
-    object.LinkEnabled = True
+    Object.LinkEnabled = True
     time.sleep(.5) #rohan's magical fix to making GuiderExpose work
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected, check if plugged in')
-        object.Quit()
+        Object.Quit()
         return
     else:
         print('Guider connected')
-    return object # has to return it so that it can be used by other commands
+    return Object # has to return it so that it can be used by other commands
 
-def guider_disconnect(object): # disconnects the MaxIm.CCDCamera object
-    object.Quit()
-    print("Guider disconnected. No object returned.")
-    return # does not return an object because its been disconnected
+def guider_disconnect(Object): # disconnects the MaxIm.CCDCamera Object
+    Object.Quit()
+    print("Guider disconnected. No Object returned.")
+    return # does not return an Object because its been disconnected
 
-def guider_expose(duration, object, auto_select = True): # takes autoguider exposure
+def guider_expose(duration, Object, auto_select = True): # takes autoguider exposure
     # this method is what initializes a guide star position
     #remember to calibrate first
 
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected. Check if plugged in.')
-        object.Quit()
+        Object.Quit()
         return
     else:
         pass
     #check moving
-    while object.GuiderMoving == True or object.GuiderRunning == True:
+    while Object.GuiderMoving == True or Object.GuiderRunning == True:
         time.sleep(1)
         continue
     time.sleep(.5) # rohan's magical fix to make GuiderExpose work
 
     # will pick the brightest star in the sky, will normally be true
     if auto_select == False:
-        object.AutoSelectStar = False
+        Object.AutoSelectStar = False
         print('AutoSelect Guide Star is turned off, so no star will be chosen.')
     else:
         pass
     
     print(f'Guider connected, exposing for {duration}s')
-    object.GuiderExpose(duration)
-    while object.GuiderMoving == True or object.GuiderRunning == True:
+    Object.GuiderExpose(duration)
+    while Object.GuiderMoving == True or Object.GuiderRunning == True:
         time.sleep(1)
         continue
     time.sleep(.5)
@@ -55,154 +55,156 @@ def guider_expose(duration, object, auto_select = True): # takes autoguider expo
     #when the coords are (0.0,0.0) you can assume that no star was found
     if auto_select == True:
         print('Checking for guide star...')
-        x = object.GuiderXStarPosition # x position in image
-        y = object.GuiderYStarPosition # y position in image
+        x = Object.GuiderXStarPosition # x position in image
+        y = Object.GuiderYStarPosition # y position in image
         time.sleep(0.5)
         print(f'Guide star coords: ({x},{y})')
-        return object
+        return Object
     else:
-        return object
+        return Object
     
-def guider_calibrate(object, duration): # calibrates the guider
+def guider_calibrate(Object, duration): # calibrates the guider
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected, check if plugged in')
-        object.Quit()
+        Object.Quit()
         return
     else:
         pass
     #check calcode
-    calcode = object.GuiderCalState
+    calcode = Object.GuiderCalState
     match calcode: # checking that calcode is appropriate, could be clunky / unnecesary
         case 0: # needs calibration
             print(f'Guider preparing to calibrate: {duration}s exposures')
-            object.GuiderCalibrate(duration)
-            while object.GuiderMoving == True or object.GuiderRunning == True:
+            Object.GuiderCalibrate(duration)
+            while Object.GuiderMoving == True or Object.GuiderRunning == True:
                 time.sleep(1)
                 continue
             time.sleep(5)
-            calcode_new = object.GuiderCalState
+            calcode_new = Object.GuiderCalState
             match calcode_new:
                 case 2:
                     print('Guider Calibrated Successfully')
-                    return object
+                    return Object
                 case 3:
                     print('Guider calibration failed')
-                    return object
+                    return Object
                 case _:
                     print('Unknown error raised')
-                    return object
+                    return Object
         case 1: # is calibrating, dont know why this would be raised
             print('Guider currently calibrating')
-            while object.GuiderMoving == True or object.GuiderRunning == True:
+            while Object.GuiderMoving == True or Object.GuiderRunning == True:
                 time.sleep(1)
                 continue
             time.sleep(5)
-            calcode_new = object.GuiderCalState
+            calcode_new = Object.GuiderCalState
             match calcode_new:
                 case 2:
                     print('Guider Calibrated Successfully')
-                    return object
+                    return Object
                 case 3:
                     print('Guider calibration failed')
-                    return object
+                    return Object
                 case _:
                     print('Unknown error raised')
-                    return object
+                    return Object
         case 2: # already calibrated
             print('Guider Calibrated Successfully')
-            return object
+            return Object
         case 3: #tried to and failed
                     print('Guider calibration failed')
-                    return object
+                    return Object
         case _: # no code that can be interpreted, just a precaution
             print('Unknown error raised')
-            return object
+            return Object
 
-def guider_calstate(object): #just checks calibration code, should be used on initial startup
+def guider_calstate(Object): #just checks calibration code, should be used on initial startup
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected, check if plugged in')
-        object.Quit()
+        Object.Quit()
         return
     else:
-        calcode = object.GuiderCalState
+        calcode = Object.GuiderCalState
         print(f'Guider calibration code: {calcode}')
-    return object
+    return Object
     
-def guider_stop(object): #stops guider from completing current task
+def guider_stop(Object): #stops guider from completing current task
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected, check if plugged in')
-        object.Quit()
+        Object.Quit()
         return
     else:
         pass
-    
-    #check moving
-    if object.GuiderMoving == True or object.GuiderRunning == True:
-        print('Stopping guider exposure/tracking')
-        object.GuiderStop()
-        return object
-    else:
-        print('Guider idle, awaiting further commands')
-        return object
 
-def cam_status(object): #gets status code for camera, decode table in scripting manual
+    #Object.GuiderStop()
+    time.sleep(0.5)
+
+    #check moving
+    if Object.GuiderRunning == True or Object.GuiderMoving == True:
+        print("Error: Guider still operating")
+        return Object
+    else:
+        print("Guider stopped, now idle")
+        return Object
+
+def cam_status(Object): #gets status code for camera, decode table in scripting manual
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected, check if plugged in')
-        object.Quit()
+        Object.Quit()
         return
     else:
         pass
     #check guider moving
-    while object.GuiderMoving == True or object.GuiderRunning == True:
+    while Object.GuiderMoving == True or Object.GuiderRunning == True:
         time.sleep(1)
         continue
 
     #check camera 1 status if it applies to the situation
-    status = object.CameraStatus
+    status = Object.CameraStatus
     print(f'Camera 1 Status Code: {status}')
-    return object
+    return Object
 
-def guider_track(object, duration): # initializes guidance of star w/ input expose duration
+def guider_track(Object, duration): # initializes guidance of star w/ input expose duration
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected, check if plugged in')
-        object.Quit()
+        Object.Quit()
         return
     else:
         pass
     #check moving
-    while object.GuiderMoving == True or object.GuiderRunning == True:
+    while Object.GuiderMoving == True or Object.GuiderRunning == True:
         time.sleep(1)
         continue
     
-    x = object.GuiderXStarPosition # x position in image
-    y = object.GuiderYStarPosition # y position in image
+    x = Object.GuiderXStarPosition # x position in image
+    y = Object.GuiderYStarPosition # y position in image
     print('Beginning guidestar tracking')
     print(f'Guidestar coords: ({x}, {y})')
 
-    object.GuiderTrack(duration)
+    Object.GuiderTrack(duration)
     #check moving
-    return object
+    return Object
 
-def guidestar_coords(object): #just prints the guidestar coords in case one is curious
+def guidestar_coords(Object): #just prints the guidestar coords in case one is curious
     #check link
-    if object.LinkEnabled != True:
+    if Object.LinkEnabled != True:
         print('Guider not connected, check if plugged in')
-        object.Quit()
+        Object.Quit()
         return
     else:
         pass
     #check moving
-    while object.GuiderMoving == True or object.GuiderRunning == True:
+    while Object.GuiderMoving == True or Object.GuiderRunning == True:
         time.sleep(1)
         continue
     time.sleep(0.5)
-    x = object.GuiderXStarPosition # x position in image
-    y = object.GuiderYStarPosition # y position in image
+    x = Object.GuiderXStarPosition # x position in image
+    y = Object.GuiderYStarPosition # y position in image
     time.sleep(0.5)
     print(f'Guide star coords: ({x},{y})')
-    return object
+    return Object
